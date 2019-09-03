9327cbc19a7513c3f6b1c74b71051202fbfe9e5a
RaZoR <razorbohan@yahoo.com>
2019-09-04 02:51:16 +0300

    add L2.2-P1-2

---
+++ L2.2-P1-2 MVC/ConsoleHttpWebServer/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ConsoleHttpWebServer.Controllers
+{
+    abstract class BaseController
+    {
+        public abstract string Handle(HttpListenerRequest request, HttpListenerResponse response);
+
+        protected string GetView(string viewName)
+        {
+            return "";
+        }
+    }
+}
