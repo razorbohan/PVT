3f95fa5ed2fc45225ee11bab15401a2af1a70d67
RaZoR <razorbohan@yahoo.com>
2019-10-06 23:42:49 +0300

    L6-P1-4 start

---
+++ L6-P1-4 ViewsAndLayout/L4-P1-5/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace L4_P1_5
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ L6-P1-4 ViewsAndLayout/L4-P1-5/ViewModel/VoteViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace L4_P1_5.ViewModel
+{
+    public class VoteViewModel
+    {
+        public int PartyId { get; set; }
+        public string Name { get; set; }
+        public string IsAttend { get; set; }
+    }
+}
