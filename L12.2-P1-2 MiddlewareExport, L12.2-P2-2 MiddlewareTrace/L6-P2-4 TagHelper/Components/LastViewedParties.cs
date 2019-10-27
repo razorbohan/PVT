1b695ce4e2b41b0517662b840fe0d7502706c62b
RaZoR <razorbohan@yahoo.com>
2019-10-27 11:51:54 +0300

    add MiddlewateExport/Trace

---
+++ L12.2-P1-2 MiddlewareExport, L12.2-P2-2 MiddlewareTrace/L6-P2-4 TagHelper/Components/LastViewedParties.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using L6_P2_4_TagHelper.Logic;
+using L6_P2_4_TagHelper.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace L6_P2_4_TagHelper.Components
+{
+    public class LastViewedParties : ViewComponent
+    {
+        private IPartyService PartyService { get; }
+
+        public LastViewedParties(IPartyService partyService)
+        {
+            PartyService = partyService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var partiesString = HttpContext.Session.GetString("last_viewed_parties");
+            if (partiesString != null)
+            {
+                var partiesIds = JsonConvert.DeserializeObject<Queue<int>>(partiesString);
+                var parties = PartyService.GetIncomingParties().Where(x => partiesIds.Contains(x.Id));
+
+                return View(parties);
+            }
+
+            return View(new List<Party>());
+        }
+    }
+}
---
+++ L12.2-P1-2 MiddlewareExport, L12.2-P2-2 MiddlewareTrace/L6-P2-4 TagHelper/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using L6_P2_4_TagHelper.Logic;
+
+namespace L6_P2_4_TagHelper.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IPartyService PartyService { get; set; }
+
+        public HomeController(IPartyService partyService)
+        {
+            PartyService = partyService;
+        }
+
+        public ActionResult Index()
+        {
+            var parties = PartyService.GetIncomingParties();
+
+            return View("Index", parties);
+        }
+    }
+}
