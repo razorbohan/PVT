7c44ce517b621168c19ffc686c08f391a2cc182d
RaZoR <razorbohan@yahoo.com>
2019-10-31 16:34:12 +0300

    add L13-P3/3-Filters. Кэширование.

---
+++ L13-P3-3 Filters. Кэширование/L6-P2-4 TagHelper/DAL/Models/Participant.cs
@@ -0,0 +1,14 @@
+﻿namespace L6_P2_4_TagHelper.Models
+{
+    public class Participant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsAttend { get; set; }
+        public string Avatar { get; set; }
+        public string Reason { get; set; }
+        public int PartyId { get; set; }
+
+        public Party Party { get; set; }
+    }
+}
