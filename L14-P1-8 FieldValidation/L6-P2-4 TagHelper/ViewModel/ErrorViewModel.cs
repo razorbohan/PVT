08ca65e6b45c8d558591b61514776849cf7c126f
RaZoR <razorbohan@yahoo.com>
2019-11-12 19:48:44 +0300

    add L14-P1-8 FieldValidation, L14-P2-8 ValidationSummary

--- L14-P1-8 FieldValidation/L6-P2-4 TagHelper/ViewModel/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace L6_P2_4_TagHelper.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
