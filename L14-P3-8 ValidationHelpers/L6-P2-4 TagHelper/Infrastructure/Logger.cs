8e5d813055178b2caf95a7d26c1d07c696adbada
RaZoR <razorbohan@yahoo.com>
2019-11-12 19:56:02 +0300

    start L14-P3-8 ValidationHelpers

---
+++ L14-P3-8 ValidationHelpers/L6-P2-4 TagHelper/Infrastructure/Logger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace L6_P2_4_TagHelper.Infrastructure
+{
+    public interface ILogger
+    {
+        void Info(string message);
+        void Warn(string message);
+        void Error(string message);
+    }
+
+    public class Logger : ILogger
+    {
+        private string LogPath { get; }
+        private readonly IHostingEnvironment _env;
+
+        public Logger(IHostingEnvironment env)
+        {
+            _env = env;
+            LogPath = $"{_env.ContentRootPath}/log.txt";
+        }
+
+        public Logger(string logPath)
+        {
+            LogPath = logPath;
+        }
+
+        public void Error(string message)
+        {
+            File.AppendAllText(LogPath, $"{DateTime.Now} ERROR {message}{Environment.NewLine}");
+        }
+
+        public void Info(string message)
+        {
+            File.AppendAllText(LogPath, $"{DateTime.Now} INFO {message}{Environment.NewLine}");
+        }
+
+        public void Warn(string message)
+        {
+            File.AppendAllText(LogPath, $"{DateTime.Now} WARN {message}{Environment.NewLine}");
+        }
+    }
+}
