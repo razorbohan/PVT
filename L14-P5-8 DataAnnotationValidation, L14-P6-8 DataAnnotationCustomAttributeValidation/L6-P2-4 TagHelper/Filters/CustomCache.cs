2b0c12d575e7680d28528f7260ad1fa40107d98c
RaZoR <razorbohan@yahoo.com>
2019-11-26 20:27:12 +0300

    add L14-P5-8, L14-P6-8

---
+++ L14-P5-8 DataAnnotationValidation, L14-P6-8 DataAnnotationCustomAttributeValidation/L6-P2-4 TagHelper/Filters/CustomCache.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace L6_P2_4_TagHelper.Filters
+{
+
+    public class CustomCacheAttribute : Attribute, IActionFilter
+    {
+        private readonly int _duration;
+        private static Dictionary<string, (DateTime Time, ViewResult Content)> CacheDict { get; set; }
+        private string _cacheKey;
+
+        public CustomCacheAttribute(int duration)
+        {
+            _duration = duration;
+
+            CacheDict = new Dictionary<string, (DateTime, ViewResult)>();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            _cacheKey = context.HttpContext.Request.Path.ToString();
+            if (CacheDict.ContainsKey(_cacheKey))
+            {
+                var cachedValue = CacheDict[_cacheKey];
+                if ((DateTime.Now - cachedValue.Time).TotalSeconds < _duration)
+                    context.Result = cachedValue.Content;
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (!string.IsNullOrEmpty(_cacheKey))
+            {
+                if (context.Result is ViewResult result)
+                {
+                    CacheDict[_cacheKey] = (DateTime.Now, result);
+                }
+            }
+        }
+    }
+}
---
+++ L14-P5-8 DataAnnotationValidation, L14-P6-8 DataAnnotationCustomAttributeValidation/L6-P2-4 TagHelper/ViewModel/VoteViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using L6_P2_4_TagHelper.Validators;
+
+namespace L6_P2_4_TagHelper.ViewModel
+{
+    public class VoteViewModel
+    {
+        public int PartyId { get; set; }
+
+        [Required(ErrorMessage = "Please, give us your name!")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Your name is too long!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please, give us your email!")]
+        [EmailValidator(ErrorMessage = "Wrong email")]
+        public string Email { get; set; }
+
+        [BooleanValidator(ErrorMessage = "Must be only 'yes' or 'no'!")]
+        public bool IsAttend { get; set; }
+    }
+}
