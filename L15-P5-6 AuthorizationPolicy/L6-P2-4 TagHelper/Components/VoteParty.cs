e6ed16cfaba7570bb0cf49e35c18f8d6d7720635
RaZoR <razorbohan@yahoo.com>
2020-01-08 12:07:33 +0300

    add L15-P5-6 AuthorizationPolicy

---
+++ L15-P5-6 AuthorizationPolicy/L6-P2-4 TagHelper/Components/VoteParty.cs
@@ -0,0 +1,13 @@
+﻿using L6_P2_4_TagHelper.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L6_P2_4_TagHelper.Components
+{
+    public class VoteParty : ViewComponent
+    {
+        public IViewComponentResult Invoke(int partId)
+        {
+            return View(new VoteViewModel { PartyId = partId });
+        }
+    }
+}
